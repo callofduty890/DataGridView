cc2adb1b193ca4d68dfdb8afd8cc06f4d02329d3
俊华 蔡 <1032031912@qq.com>
2020-10-06 09:31:08 +0800

    表格控件

--- 10_6_代码/Form1.Designer.cs
+++ 10_6_代码/Form1.Designer.cs
@@ -39,76 +39,89 @@
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Top;
             this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 27;
-            this.dataGridView1.Size = new System.Drawing.Size(695, 386);
+            this.dataGridView1.Size = new System.Drawing.Size(521, 309);
             this.dataGridView1.TabIndex = 0;
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("微软雅黑", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.button1.Location = new System.Drawing.Point(13, 402);
+            this.button1.Location = new System.Drawing.Point(10, 322);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(129, 51);
+            this.button1.Size = new System.Drawing.Size(97, 41);
             this.button1.TabIndex = 1;
             this.button1.Text = "插入表头";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("微软雅黑", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.button2.Location = new System.Drawing.Point(148, 402);
+            this.button2.Location = new System.Drawing.Point(111, 322);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(129, 51);
+            this.button2.Size = new System.Drawing.Size(97, 41);
             this.button2.TabIndex = 1;
             this.button2.Text = "插入一行";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
             this.button3.Font = new System.Drawing.Font("微软雅黑", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.button3.Location = new System.Drawing.Point(283, 402);
+            this.button3.Location = new System.Drawing.Point(212, 322);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(129, 51);
+            this.button3.Size = new System.Drawing.Size(97, 41);
             this.button3.TabIndex = 1;
             this.button3.Text = "删除一行";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
             this.button4.Font = new System.Drawing.Font("微软雅黑", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.button4.Location = new System.Drawing.Point(418, 402);
+            this.button4.Location = new System.Drawing.Point(314, 322);
+            this.button4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(129, 51);
+            this.button4.Size = new System.Drawing.Size(97, 41);
             this.button4.TabIndex = 1;
             this.button4.Text = "修改一行";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button5
             // 
             this.button5.Font = new System.Drawing.Font("微软雅黑", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.button5.Location = new System.Drawing.Point(553, 402);
+            this.button5.Location = new System.Drawing.Point(415, 322);
+            this.button5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(129, 51);
+            this.button5.Size = new System.Drawing.Size(97, 41);
             this.button5.TabIndex = 1;
             this.button5.Text = "删除指定";
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(695, 469);
+            this.ClientSize = new System.Drawing.Size(521, 375);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
--- 10_6_代码/Form1.cs
+++ 10_6_代码/Form1.cs
@@ -12,9 +12,74 @@ namespace _10_6_代码
 {
     public partial class Form1 : Form
     {
+        //1.新建数据类型:DataTable[储存在内存条中的表格数据类型]
+        DataTable dt = new DataTable();
+
         public Form1()
         {
             InitializeComponent();
+            this.dataGridView1.DataSource = dt;//将控件的数据源绑定Datatable
+        }
+        //创建表头信息
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //学号
+            dt.Columns.Add("学号", typeof(int));
+            //姓名
+            dt.Columns.Add("姓名", typeof(string));
+            //班级
+            dt.Columns.Add("班级", typeof(string));
+            //电话号码
+            dt.Columns.Add("电话号码", typeof(string));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dt.Rows.Add(1, "助教", "语言班", "123-456");
+            dt.Rows.Add(2, "龙傲天", "语言班", "123-456");
+            dt.Rows.Add(3, "龙嗷嗷", "语言班", "123-456");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //拿到最后一行的索引
+            int Index = dt.Rows.Count - 1;
+            //显示当前最后一行
+            for (int j = 0; j < dt.Rows.Count; j++)
+            {
+                for (int i = 0; i < dt.Rows[j].ItemArray.Length; i++)
+                {
+                    Console.Write(dt.Rows[j].ItemArray[i] + " ");
+                }
+                Console.WriteLine();
+            }
+            //删除最后一行
+            if (Index >= 0)
+            {
+                dt.Rows.Remove(dt.Rows[Index]);
+            }
+            else
+            {
+                MessageBox.Show("已删除完毕!", "提示消息", MessageBoxButtons.YesNo);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //拿到最后一行的索引
+            int Index = dt.Rows.Count - 1;
+            //修改修后一行
+            dt.Rows[Index][1] = "佳佳";
+            dt.Rows[Index][2] = "三年二班";
+            dt.Rows[Index][3] = "089-789";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //拿到活跃单元格的索引
+            int Index = this.dataGridView1.CurrentCell.RowIndex;
+            //删除指定的活跃单元格
+            dt.Rows.Remove(dt.Rows[Index]);
         }
     }
 }
